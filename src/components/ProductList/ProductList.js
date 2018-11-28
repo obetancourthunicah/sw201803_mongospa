f761da72bb5d9a40ea2b49f577cf3740c0ae07bd
Orlando J Betancourth <orlando.betancourth@gmail.com>
2018-11-27 18:56:22 -0600

    Arreglos de Nodos para listas e impementacion de axios para obtener los datos del API

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -1,17 +1,50 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+const ListItem = (props) => {
+  return(
+    <li>
+    <h3>{props.descripcion}</h3>
+    <b>{props.codigo}</b>
+      <Link to={`/producto/${props._id}`}>Ver</Link>
+    </li>
+  )
+}
 
 export default class ProductList extends Component {
   constructor(){
     super();
+    this.state = {
+      prods: []
+    }
+    this.componentDidMount = this.componentDidMount.bind(this);
+  } // constructor
+  componentDidMount(){
+    axios.get('/api/productos/all/20/1')
+      .then(
+        (response)=>{
+          let data = response.data;
+          this.setState({"prods": data});
+        }
+      )
+      .catch(
+          function(error){
+            console.log(error);
+          }
+      ); // axios
   }
   render(){
+    let productItems = this.state.prods.map(
+      (o, i) => {
+          return (<ListItem  {...o} key={o._id} />);
+      }
+    );
     return (
       <section>
         <p>Estamos en Product List</p>
         <ul>
-        <li><Link to="/product/codigo1">Producto 1</Link></li>
-        <li><Link to="/product/codigo2">Producto 2</Link></li>
+          {productItems}
         </ul>
       </section>
     )
